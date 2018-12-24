a2a81be550120205df2a860486a88fb30f570e84
Mark Szymczyk <mark@meandmark.com>
2018-12-23 22:28:22 -0500

    Initial Commit

---
+++ DraggableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DraggableView
+//
+//  Created by mark on 12/23/18.
+//  Copyright © 2018 Swift Dev Journal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
